e4e40ccb55465675adbf8805b45de1de42975543
andreylol17 <117731324+andreylol17@users.noreply.github.com>
2022-11-16 00:26:30 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,22 @@
+﻿const n=5;
+begin
+  var m:array[1..n]of integer;
+var min:Integer;
+var k:integer;
+var i:integer;
+ print ('Введите числа ');
+ for i:=1 to n do read(m[i]);
+ println;
+ min+=m[1];
+ k+=1;
+ for i:=2 to n do 
+  if m[i]=min then inc(k)
+ else if m[i]<min then
+ begin
+  min+=m[i];
+  k+=1;
+ end;
+ print('минимальный элемент = ',min);
+ print('Кол-во вхождений = ',k);
+ println;
+end.
---
+++ 2.pas
@@ -0,0 +1,30 @@
+﻿begin
+  var a: string;
+  var b: string;
+  var i: integer;
+  var j: integer;
+  var flag: boolean;
+  var k: integer;
+  Print('Введите строку: ');
+  Readln(a);
+  Print('Введите подстроку: ');
+  Readln(b);
+  k:= 0;
+  for i:= 1 to length(a) do
+    begin
+      flag:= false;
+      for j:= 1 to length(b) do
+        if a[i-1+j] <> b[j] then
+          begin
+            flag:= true;
+            break;
+          end;
+      if not flag then
+        inc(k);
+    end;
+  if k <> 0 then
+    Print('Кол-во вхождений: ',k)
+  else
+    Print('Вхождений необнаруженно');
+  Readln;
+end.
